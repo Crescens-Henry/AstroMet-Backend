ce9617bfe3a7a3f9583f8d266057fe82b9d51211
Crescens <crescencio.ps.code@gmail.com>
2023-08-15 09:50:55 -0600

    first commit complete

---
+++ src/main/java/com/ApiRest/AstroMet/AstroMetApplication.java
@@ -0,0 +1,13 @@
+package com.ApiRest.AstroMet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroMetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroMetApplication.class, args);
+	}
+
+}
